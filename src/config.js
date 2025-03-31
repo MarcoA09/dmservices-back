fa21b997c03c6acb8aa2c0b13e9ad3b7f3280404
MarcoA09 <marcoa.marquez06@gmail.com>
2025-03-30 20:04:44 -0600

    Delete src/config.js

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = process.env.PORT || 3000;
-
-
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://marcoamarquez06:GH8qssi9v82tPtam@desweb.d3qth.mongodb.net/DmServices?retryWrites=true&w=majority&appName=DesWeb";
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
-export const FRONTEND_URL = "http://localhost:5173"
