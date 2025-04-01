16f787c2e27969b6390b87c14542b5876dd974b5
MarcoA09 <marcoa.marquez06@gmail.com>
2025-04-01 00:24:05 -0600

    Add files via upload

---
+++ src/utils/email.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+export const sendVerificationEmail = async (email, verificationLink) => {
+  try {
+   
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "2025178001@uteq.edu.mx",
+            pass: "*Ma06dp27*0609",
+      },
+    });
+
+    const mailOptions = {
+      from: `"DM Eventos" <${"2025178001@uteq.edu.mx"}>`,
+      to: email,
+      subject: "Verifica tu cuenta",
+      html: `
+        <h3>Hola,</h3>
+        <p>Gracias por registrarte en nuestro sitio. Para verificar tu correo, haz clic en el siguiente enlace:</p>
+        <a href="${verificationLink}" target="_blank">Verificar mi correo</a>
+        <p>Si no solicitaste esto, ignora este mensaje.</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log("Correo de verificación enviado a:", email);
+  } catch (error) {
+    console.error("Error enviando correo:", error);
+    throw new Error("No se pudo enviar el correo de verificación");
+  }
+};
