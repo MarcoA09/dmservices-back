16321f50b02f87cae302451c7fcdc0796fa636a4
MarcoA09 <marcoa.marquez06@gmail.com>
2025-04-01 00:21:12 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,12 @@
 import admin from "firebase-admin";
-import { readFile } from "fs/promises";
 
-const serviceAccount = JSON.parse(
-  await readFile(
-    new URL("./dmapp-85b06-firebase-adminsdk-fbsvc-33cb0691d6.json", import.meta.url)
-  )
-);
+const serviceAccountBase64 = process.env.FIREBASE_CREDENTIALS_BASE64;
+
+
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, "base64").toString("utf-8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-
-export { admin }; 
+export { admin };
